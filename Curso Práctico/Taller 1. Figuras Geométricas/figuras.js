f4b70a5bb33a62c266e41013a6ac43d4e9ec4798
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-09-25 20:01:19 -0500

    Construyendo la lógica de las formulas

--- Curso Práctico/Taller 1. Figuras Geométricas/figuras.js
+++ Curso Práctico/Taller 1. Figuras Geométricas/figuras.js
@@ -1,1 +1,50 @@
-alert("Hello!")
+// Cuadrado
+
+let ladoCuadrado = parseInt(prompt('Cuando mide cada lado del cuadrado?'))
+console.log(`Cada lado mide ${ladoCuadrado} unidades.`)
+
+let perimetroCuadrado = ladoCuadrado * 4
+console.log(`El perimetro del cuadrado es de ${perimetroCuadrado} unidades.`)
+
+let areaCuadrado = ladoCuadrado**2
+console.log(`El área del cuadrado es de ${areaCuadrado} unidades cuadradas.`)
+
+// let cuadrado = document.getElementById('Cuadrado')
+// cuadrado.innerHTML=ladoCuadrado
+
+// Triangulo
+
+let baseTriangulo = parseInt(prompt('Cuanto mide la base del triangulo?'))
+console.log(`La base del triangulo mide ${baseTriangulo} unidades.`)
+
+let ladoTriangulo = parseInt(prompt('Cuanto mide uno de los lados del triangulos?'))
+let lado2Triangulo =  10
+
+let alturaTriangulo = Math.sqrt(ladoTriangulo**2 - (baseTriangulo/2)**2)
+
+console.log(Math.sqrt(alturaTriangulo))
+
+console.log(`Los otros lados del triangulo miden ${baseTriangulo} y ${lado2Triangulo} unidades.`)
+
+let perimetroTriangulo = baseTriangulo + ladoTriangulo + lado2Triangulo
+console.log(`El perimetro del triangulo es de ${perimetroTriangulo} unidades.`)
+
+let areaTriangulo = (baseTriangulo * alturaTriangulo)/2 
+console.log(`El área del triangulo es de ${areaTriangulo} unidades cuadradas.`)
+
+// Circulo 
+
+let lado_circulo
+
+
+
+
+
+
+
+
+
+
+
+
+
