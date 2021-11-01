45574f7c8ad57bbc22b35af41a562c6e87b17076
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-10-31 19:38:34 -0500

    Utilizando funciones

--- Curso Práctico/Taller 1. Figuras Geométricas/figuras.js
+++ Curso Práctico/Taller 1. Figuras Geométricas/figuras.js
@@ -1,53 +1,49 @@
 // Cuadrado
 
+function cuadrado(lado) {
+    var area = lado * lado;
+    var perimetro = lado * 4;
+    return [area, perimetro];
+}
+
+function triangulo(base, altura) {
+    var area = (base * altura) / 2;
+    var perimetro = base * 3;
+    return [area, perimetro];
+}
+
+function circulo(radio) {
+    var area = Math.PI * (radio * radio);
+    var perimetro = 2 * Math.PI * radio;
+    return [area, perimetro];
+}
+
+// Cuadrado
+
 console.group('Cuadrado')
-let ladoCuadrado = parseInt(prompt('Cuando mide cada lado del cuadrado?'))
-console.log(`Cada lado mide ${ladoCuadrado} unidades.`)
 
-let perimetroCuadrado = ladoCuadrado * 4
-console.log(`El perimetro del cuadrado es de ${perimetroCuadrado} unidades.`)
+    console.log(`El área del cuadrado es de ${cuadrado(10)[0]} unidades.`)
+    console.log(`El perimetro del cuadrado es de ${cuadrado(10)[1]} unidades.`)
 
-let areaCuadrado = ladoCuadrado**2
-console.log(`El área del cuadrado es de ${areaCuadrado} unidades cuadradas.`)
 console.groupEnd()
-// let cuadrado = document.getElementById('Cuadrado')
-// cuadrado.innerHTML=ladoCuadrado
 
-// Triangulo 
+// Triangulo
 
-console.group('Triángulo')
-let baseTriangulo = parseInt(prompt('Cuanto mide la base del triangulo?'))
-console.log(`La base del triangulo mide ${baseTriangulo} unidades.`)
+console.group('Triangulo')
 
-let ladoTriangulo = parseInt(prompt('Cuanto mide uno de los lados del triangulos?'))
-let ladoTriangulo2 =  ladoTriangulo
+    console.log(`El área del triangulo es de ${triangulo(10, 5)[0]} unidades.`)
+    console.log(`El perimetro del triangulo es de ${triangulo(10, 5)[1]} unidades.`)
 
-let alturaTriangulo = Math.sqrt(ladoTriangulo**2 - (baseTriangulo/2)**2)
-
-console.log(`La altura del triangulo es de ${alturaTriangulo.toFixed(2)} unidades`)
-console.log(`Los otros lados del triangulo miden ${ladoTriangulo} unidades cada uno.`)
-
-let perimetroTriangulo = baseTriangulo + ladoTriangulo + ladoTriangulo2
-console.log(`El perimetro del triangulo es de ${perimetroTriangulo} unidades.`)
-
-let areaTriangulo = (baseTriangulo * alturaTriangulo)/2 
-console.log(`El área del triangulo es de ${areaTriangulo.toFixed(2)} unidades cuadradas.`)
 console.groupEnd()
 
 // Circulo
 
-let radioCirculo = parseInt(prompt('Cuánto mide el radio del circulo?'))
-
-let areaCirculo = Math.PI * (radioCirculo**2)
-let perimetroCirculo = 2 * Math.PI * radioCirculo
-
 console.group('Circulo')
 
-console.log(`El área del circulo es de ${areaCirculo.toFixed(2)} unidades`)
-console.log(`El perimetro del circulo es de ${perimetroCirculo.toFixed(2)} unidades`)
+    console.log(`El área del circulo es de ${circulo(10)[0].toFixed(2)} unidades.`)
+    console.log(`El perimetro del circulo es de ${circulo(10)[1].toFixed(2)} unidades.`)
 
 console.groupEnd()
-
 
 
 
